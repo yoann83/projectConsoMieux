27721d2a212d0d563a2f6e900da8cb6e028b82c4
Martinez yoann <41195157+yoann83@users.noreply.github.com>
2022-10-24 13:16:52 +0200

    add logo mini

--- src/App.js
+++ src/App.js
@@ -3,14 +3,27 @@ import "./styles.scss";
 export default function App() {
   return (
     <div className="App">
-      <div className="box-title">
+      <div className="box-logo-xs">
+        <img
+          src="https://raw.githubusercontent.com/yoann83/media/main/consoMieuxMiniFini.png"
+          width="250"
+        />
+        <img
+          src="https://raw.githubusercontent.com/yoann83/media/main/consoMieuxMiniFiniBleu.png"
+          width="250"
+        />
+        <hr />
+      </div>
+      <div className="box-logo-lg">
         <img
           src="https://raw.githubusercontent.com/yoann83/media/main/consomieuxFini.png"
-          width="300"
+          width="500"
         />
       </div>
+      <hr />
       <div>
-        <h1>test</h1>
+        <h1>ACCUEIL</h1>
+        <hr />
         <button className="btn-cos-red" type="button">
           Enregistrer
         </button>
